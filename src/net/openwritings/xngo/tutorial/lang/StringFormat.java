8e9422c8e4c940bf4d79b8b5c283cec9460b743f
limelime <limelime@users.noreply.github.com>
2019-02-22 16:56:21 +0000

    add

---
+++ src/net/openwritings/xngo/tutorial/lang/StringFormat.java
@@ -0,0 +1,20 @@
+package net.openwritings.xngo.tutorial.lang;
+
+public class StringFormat {
+
+	public static void main(String[] args) {
+		
+		String name="Joe";
+		int age = 23;
+		float weight = 179.24f;
+		double salary = 76900.33;
+		
+		String s="row results=> name="+name+", age="+age+", weight="+weight+", salary="+salary;
+		System.out.println(s);
+		
+		s=String.format("row results=> name=%s, age=%d, weight=%s, salary=%s", name, age, weight, salary);
+		System.out.println(s);
+		
+	}
+
+}
