365dbc1b66ece87f05109f2a0942b1978f0c9a33
limelime <limelime@users.noreply.github.com>
2019-02-24 17:03:57 +0000

    Add radnom number.

---
+++ src/net/openwritings/xngo/tutorial/math/RandomNumber.java
@@ -0,0 +1,18 @@
+package net.openwritings.xngo.tutorial.math;
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+		
+		int a=4, b=8;
+		int rndInt = getRandomInt(a, b);
+		System.out.println(rndInt);
+	}
+	public static int getRandomInt(int a, int b){
+		
+		int range = b-a;
+		double rndDouble = Math.random()*range;
+		
+		return Double.valueOf(rndDouble).intValue()+a;
+	}
+}
