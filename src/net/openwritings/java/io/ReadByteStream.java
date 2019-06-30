c143313a3d02a790ae3e034c0910b77dae1ef9f9
limelime <limelime@users.noreply.github.com>
2019-06-30 20:47:03 +0000

    Format.

--- src/net/openwritings/java/io/ReadByteStream.java
+++ src/net/openwritings/java/io/ReadByteStream.java
@@ -3,73 +3,65 @@ package net.openwritings.java.io;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-/**
- * Description: Read data stream into buffer for the beginning, the middle and the end of the stream.
- * @author Xuan Ngo
- */
-public class ReadByteStream
-{
+public class ReadByteStream {
 
-  public static void main(String[] args)
-  {
-    int buffer_size = 5;
+	public static void main(String[] args) {
+		int buffer_size = 5;
 
-    byte[] buffer = new byte[buffer_size];
-    try
-    {
-      // Assuming filename.txt does exist. Tested with file containing letters from a to z.
-      InputStream inputStream = new FileInputStream("./filename.txt");
-      
-      // Total length of stream.
-      int total_length = inputStream.available();
+		byte[] buffer = new byte[buffer_size];
+		try {
+			// Assuming filename.txt does exist. Tested with file containing letters from a
+			// to z.
+			InputStream inputStream = new FileInputStream("./filename.txt");
 
-      //*** Beginning buffer
-      inputStream.read(buffer);       // read stream data into buffer
-      displayBuffer("Beginning: ", buffer);
+			// Total length of stream.
+			int total_length = inputStream.available();
 
-      //*** Middle buffer
-      int skip_n_to_middle = (total_length / 2) - (buffer_size / 2) - buffer_size;
-      System.out.println("\tWill skip the next " + skip_n_to_middle + " bytes.");
-      inputStream.skip(skip_n_to_middle);
-      inputStream.read(buffer);
-      displayBuffer("Middle: ", buffer);
+			// *** Beginning buffer
+			inputStream.read(buffer); // read stream data into buffer
+			displayBuffer("Beginning: ", buffer);
 
-      //*** Last buffer
-      int skip_n_to_last = total_length - buffer_size - skip_n_to_middle - buffer_size - buffer_size;
-      System.out.println("\tWill skip the next " + skip_n_to_last + " bytes.");
-      inputStream.skip(skip_n_to_last);
-      inputStream.read(buffer);
-      displayBuffer("Last: ", buffer);
+			// *** Middle buffer
+			int skip_n_to_middle = (total_length / 2) - (buffer_size / 2) - buffer_size;
+			System.out.println("\tWill skip the next " + skip_n_to_middle + " bytes.");
+			inputStream.skip(skip_n_to_middle);
+			inputStream.read(buffer);
+			displayBuffer("Middle: ", buffer);
 
-      System.out.println("END"); // Use as a marker to see non-visible character(e.g. newline).
-      
-      inputStream.close();
+			// *** Last buffer
+			int skip_n_to_last = total_length - buffer_size 
+										- skip_n_to_middle - buffer_size - buffer_size;
+			System.out.println("\tWill skip the next " + skip_n_to_last + " bytes.");
+			inputStream.skip(skip_n_to_last);
+			inputStream.read(buffer);
+			displayBuffer("Last: ", buffer);
 
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
+			// Use as a marker to see non-visible character(e.g. newline).
+			System.out.println("DONE"); 
 
-  }
-  
-  // Helper to display buffer.
-  static public void displayBuffer(String text, byte[] buffer)
-  {
-    // Text to print before the buffer.
-    System.out.print(text);
-    
-    char c;
-    // For each byte in the buffer
-    for (byte b : buffer)
-    {
-      // Convert byte to character
-      c = (char) b;
+			inputStream.close();
 
-      // Print character
-      System.out.print(c);
-    }
-    System.out.println();
-  }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// Helper to display buffer.
+	static public void displayBuffer(String text, byte[] buffer) {
+		// Text to print before the buffer.
+		System.out.print(text);
+
+		char c;
+		// For each byte in the buffer
+		for (byte b : buffer) {
+			// Convert byte to character
+			c = (char) b;
+
+			// Print character
+			System.out.print(c);
+		}
+		System.out.println();
+	}
 
 }
