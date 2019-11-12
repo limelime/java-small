1c93e9778b9a371ad12084b6a3498878c4befbe5
limelime <limelime@users.noreply.github.com>
2019-11-12 15:48:52 -0500

    Rename variable to anyDate, more generic.

--- src/net/openwritings/java/time/LastDayOfMonthCalendar.java
+++ src/net/openwritings/java/time/LastDayOfMonthCalendar.java
@@ -7,16 +7,16 @@ public class LastDayOfMonthCalendar {
     public static void main(String[] args) {
     	
     	// Set a date, e.g. 2013-01-17.
-    	Calendar calendar = Calendar.getInstance();
-    	calendar.set(2013, 0, 17); // Note: Month 0 is January.
+    	Calendar anyDate = Calendar.getInstance();
+    	anyDate.set(2013, 0, 17); // Note: Month 0 is January.
     	
     	// Get the last day of the month.
-        int lastDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-        calendar.set(Calendar.DAY_OF_MONTH, lastDayOfMonth);
+        int lastDayOfMonth = anyDate.getActualMaximum(Calendar.DAY_OF_MONTH);
+        anyDate.set(Calendar.DAY_OF_MONTH, lastDayOfMonth);
         
         // SimpleDateFormat is used because 
         //	Calendar.toString() outputs more data than needed.
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(sdf.format(calendar.getTime()));
+        System.out.println(sdf.format(anyDate.getTime()));
     }
 }
