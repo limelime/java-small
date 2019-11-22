1d4d375e3041dd5597742ba1fdf6f7eb7a145deb
limelime <limelime@users.noreply.github.com>
2019-11-22 17:25:51 -0500

    update.

--- src/net/openwritings/java/string/StringFormat.java
+++ src/net/openwritings/java/string/StringFormat.java
@@ -11,6 +11,34 @@ public class StringFormat {
 		// Using String.format().
 		String sf = String.format("(%s, %s, %s)", age, qty, count);
 		System.out.println(sf);
+		
+		// -----------------
+		// ## Format string.
+		
+		// Pre-allocate 10 chars space for each variable.
+		sf = String.format("(%10s, %10s, %10s)", age, qty, count);
+		System.out.println(sf);
+
+		// Right align value.
+		sf = String.format("(%-10s, %-10s, %-10s)", age, qty, count);
+		System.out.println(sf);
+
+		
+		// -----------------
+		// ## Format number.
+		
+		// Show only 2 decimal points.
+		double avg = 3695.32756;
+		sf = String.format("%.2f", avg);
+		System.out.println(sf);
+
+		// Add thousand separator using comma(,).
+		sf = String.format("%,.2f", avg);
+		System.out.println(sf);
+	
+		// Pad zeros so the length is 9 digits long.
+		sf = String.format("%09d", count);
+		System.out.println(sf);				
 
 	}
 
