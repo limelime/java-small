0f62385730c304a23610607f2791329551b54d3a
limelime <limelime@users.noreply.github.com>
2019-11-25 12:44:56 -0500

    renamed.

---
+++ src/net/openwritings/java/time/DateTime7Current.java
@@ -0,0 +1,24 @@
+package net.openwritings.java.time;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+public class DateTime7Current {
+
+    public static void main(String[] args) {
+
+    	// Using Date class.
+        Date date = new Date();
+        System.out.println(date.toString());
+        
+        // Using Calendar class.
+        //		SimpleDateFormat is used because 
+        //		Calendar.toString() outputs more data than needed.
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        System.out.println(sdf.format(calendar.getTime()));
+
+    }
+
+}
